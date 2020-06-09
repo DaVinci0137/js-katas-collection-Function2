4423d4a5491d9c1d49823eb2c19b19287633b454
Andrew Brown <DaVinci0137@gmail.com>
2020-06-09 17:23:14 -0400

    updated main.js

--- javascript/main.js
+++ javascript/main.js
@@ -4,23 +4,31 @@ function add(x, y) {
 console.log(add(2, 4));
 
 function multiply(x, y) {
-    let answer = x
-    for (var i = 0; i < y - 1; i++) {
-        answer += x
+    let answer = 0
+    for (let i = 0; i < y; i++) {
+        answer = add(x, answer)
     }
     return answer
 }
 console.log(multiply(6, 4));
 
-function power(x, y) {
-    Math.pow(x, y);
+function power(x, n) {
+    let answer = 1
+    for (let i = 0; i < n; i++) {
+        answer = multiply(x, answer)
+    }
+    return answer
 }
 console.log(power(2, 8));
 console.log(power(3, 4));
 
 function factorial(x) {
-    return x;
+    let answer = 1
+    for (let i = 0; i > x, i++) {
+        answer = multiply(x, answer)
+    }
+    return answer
 }
-console.log(factorial(5));
+console.log(factorial(4));
 
 
